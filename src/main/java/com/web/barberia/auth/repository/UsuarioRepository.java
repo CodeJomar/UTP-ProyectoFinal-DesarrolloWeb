8bcf14b2594f74f29c2d9cb9a81eaa7693229016
CodeJomar <codejomar@gmail.com>
2025-06-07 00:07:09 -0500

    repositorio de las entidades

---
+++ src/main/java/com/web/barberia/auth/repository/UsuarioRepository.java
@@ -0,0 +1,9 @@
+package com.web.barberia.auth.repository;
+
+import com.web.barberia.auth.model.Usuario;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UsuarioRepository extends JpaRepository<Usuario, Long> {
+}
