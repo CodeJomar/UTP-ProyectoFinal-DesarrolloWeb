c4e4cb0cef8ba3f6e4b50c6d10446483dff01b8a
CodeJomar <codejomar@gmail.com>
2025-06-06 23:54:22 -0500

    agregue la tabla roles en ORM

--- src/main/java/com/web/barberia/auth/model/Rol.java
+++ src/main/java/com/web/barberia/auth/model/Rol.java
@@ -1,4 +1,39 @@
 package com.web.barberia.auth.model;
 
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "roles")
 public class Rol {
+    
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    
+    @Column(name = "nombre_rol")
+    private String nombre;
+    
+    public Rol() {
+    }
+    
+    public Rol(Long id, String nombre) {
+        this.id = id;
+        this.nombre = nombre;
+    }
+    
+    public Long getId() {
+        return id;
+    }
+    
+    public void setId(Long id) {
+        this.id = id;
+    }
+    
+    public String getNombre() {
+        return nombre;
+    }
+    
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
 }
